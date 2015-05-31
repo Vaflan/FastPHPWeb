0a4422e29c88b090fc25596debfa19347a3c0e02
Ruslans Jermakovics <vaflancher@gmail.com>
2015-05-31 14:48:41 +0300

    New features
    simplified code, added the ability to send their headers and corrected
    with the implementation of __FILE__ (__DIR__)

--- main.php
+++ main.php
@@ -5,6 +5,7 @@ define('FASTPHPWEB_CONTENT', 'web');
 define('FASTPHPWEB_MIME_TYPES', 'mime.types');
 define('FASTPHPWEB_404', '404 Object Not Found');
 define('FASTPHPWEB_TIMEZONE', 'Europe/Riga');
+define('FASTPHPWEB_EVAL_PHP', 'php,phps,phtml');
 define('FASTPHPWEB_INDEX', 'index.php');
 define('FASTPHPWEB_SHOW_AGENT', true);
 define('FASTPHPWEB_ERROR_BEEP', true);
@@ -22,7 +23,7 @@ function console_log($message) {
 			$message = str_replace('%'.$i.'%', func_get_arg($i), $message);
 		}
 	}
-	echo $log = date("[d.m.Y H:i:s] ").$message."\n";
+	echo $log = date('[d.m.Y H:i:s] ').$message."\n";
 	return $log;
 }
 function load_types($file) {
@@ -43,19 +44,32 @@ function load_types($file) {
 	}
 	return $types;
 }
+function shutdown_service() {
+	$e = error_get_last();
+	if($e == null)
+		$e = array('message' => 'No errors', 'line' => 0);
+	console_log($e['message'].' [line: '.$e['line'].']');
+}
 function create_response($content, $status, $headers=array()) {
-	return	$status. FASTPHPWEB_CRLF .
+	return $status. FASTPHPWEB_CRLF .
 			'Server: FastPHPWeb' . FASTPHPWEB_CRLF .
 			(count($headers) > 0 ? implode(FASTPHPWEB_CRLF, $headers).FASTPHPWEB_CRLF : '') .
 			'Content-Length: ' . strlen($content) . FASTPHPWEB_CRLF .
 			'Connection: close' . FASTPHPWEB_CRLF .
 			FASTPHPWEB_CRLF . $content;
 }
-function shutdown_service() {
-	$e = error_get_last();
-	if($e == null)
-		$e = array('message' => 'No errors', 'line' => 0);
-	console_log($e['message'].' [line: '.$e['line'].']');
+function response_header($header) {
+	global $response_headers;
+	$key = strtolower(current(explode(':', $header, 2)));
+	$response_headers[$key] = $header; 
+}
+function fix_php_contents($content, $what, $to) {
+	return preg_replace_callback(
+		"|<\?(?:\s[^>]*)?([^?]+)\?>|i",
+		function ($matches) use ($what, $to) {
+			return '<?'.str_replace($what, $to, $matches[1]).'?>';
+		},
+		$content);
 }
 
 
@@ -66,6 +80,7 @@ date_default_timezone_set(FASTPHPWEB_TIMEZONE);
 register_shutdown_function('FastPHPWeb\engine\shutdown_service');
 $mime_types = load_types(FASTPHPWEB_MIME_TYPES);
 $socket = stream_socket_server('tcp://0.0.0.0:'.FASTPHPWEB_PORT, $errno, $errstr);
+$memory_unit = array('b', 'kb', 'mb', 'gb', 'tb', 'pb');
 if(!$socket) {
 	die($errstr.' ('.$errno.')');
 }
@@ -95,85 +110,71 @@ while(true) {
 
 	foreach($read as $connect) { /* process all connections */
 		console_log('Connection accepted from '.stream_socket_get_name($connect, true));
-		$headers = '';
+		$response_headers = array();
+
+		$request = '';
 		while($buffer = rtrim(fgets($connect))) {
-			$headers .= $buffer."\n";
+			$request .= $buffer."\n";
 		}
 
-		$header_array = explode("\n", $headers);
-		$header_get = explode(" ", trim($header_array[0]));
-		if(!empty($header_array[0]))
-			console_log($header_array[0]);
-		elseif(count($header_array) > 1)
-			console_log($header_array);
+		$request_header_array = explode("\n", $request);
+		$header_get = explode(' ', trim($request_header_array[0]));
+		if(!empty($request_header_array[0]))
+			console_log($request_header_array[0]);
+		elseif(count($request_header_array) > 1)
+			console_log($request_header_array);
 
 		/* show User Agent */
-		if(FASTPHPWEB_SHOW_AGENT && count($header_array) > 1) {
-			foreach($header_array AS $header_row) {
-				$header_row = explode(':', trim($header_row), 2);
-				$header_row[0] = strtolower($header_row[0]);
-				if(strstr($header_row[0], 'host')) {
-					console_log('Host: '.trim($header_row[1]));
-				}
-				else if(strstr($header_row[0], 'agent')) {
-					console_log('User-Agent: '.trim($header_row[1]));
+		if(FASTPHPWEB_SHOW_AGENT && count($request_header_array) > 1) {
+			foreach($request_header_array AS $request_header_row) {
+				$request_header_row = explode(':', trim($request_header_row), 2);
+				switch(strtolower($request_header_row[0])) {
+					case 'host':
+						console_log('Host: '.trim($request_header_row[1]));
+						break;
+					case 'user-agent':
+						console_log('User-Agent: '.trim($request_header_row[1]));
+						break;
 				}
 			}
 		}
 
 		if(count($header_get) > 2 && strstr(strtolower($header_get[2]), 'http/1.1')) {
-			if(strtolower($header_get[0]) == 'get') {
-				$header_get[1] = current(explode('?', $header_get[1]));
-				if($header_get[1] == '/' || $header_get[1] == '\\') {
-					$header_get[1] = FASTPHPWEB_INDEX;
+			if(in_array(strtolower($header_get[0]), array('get', 'post'))) {
+				$location = current(explode('?', $header_get[1], 2));
+				if(substr(str_replace('\\', '/', $location), -1) == '/') {
+					$location .= FASTPHPWEB_INDEX;
 				}
+				$location = str_replace('/', DIRECTORY_SEPARATOR, $location);
+				if(substr(DIRECTORY_SEPARATOR, 0 , 1) != DIRECTORY_SEPARATOR)
+					$location = DIRECTORY_SEPARATOR.$location;
 
-				$get_request = FASTPHPWEB_CONTENT.DIRECTORY_SEPARATOR.$header_get[1];
+				$get_request = FASTPHPWEB_CONTENT.$location;
+				$current_file_location = dirname(__FILE__).DIRECTORY_SEPARATOR.FASTPHPWEB_CONTENT.$location;
 				if(is_file($get_request)) {
-					$type = explode('.', $get_request);
-					$type = end($type);
+					$type = array_pop(explode('.', $get_request));
 					$content = file_get_contents($get_request);
-					$headers = array();
-					if($type == 'php') {
+					if(in_array($type, explode(',', FASTPHPWEB_EVAL_PHP))) {
+						$content = fix_php_contents($content, '__FILE__', '\''.$current_file_location.'\'');
+						$content = fix_php_contents($content, '__DIR__', '\''.dirname($current_file_location).'\'');
+						$content = fix_php_contents($content, array(' header(', "\n".'header(', "\t".'header('), array(' \FastPHPWeb\engine\response_header(', "\n".'\FastPHPWeb\engine\response_header(', "\t".'\FastPHPWeb\engine\response_header('));
 						$remote_addr = explode(':', stream_socket_get_name($connect, true), 2);
 						$_SERVER['REMOTE_PORT'] = array_pop($remote_addr);
 						$_SERVER['REMOTE_ADDR'] = current($remote_addr);
 						$_SERVER['SERVER_PORT'] = FASTPHPWEB_PORT;
-						if(count($header_array) > 1) {
-							foreach($header_array AS $header_row) {
-								$header_row = explode(':', trim($header_row), 2);
-								if(count($header_row) > 1) {
-									$header_row[1] = trim($header_row[1]);
-									switch(strtolower(trim($header_row[0]))) {
-										case 'host':
-											$_SERVER['HTTP_HOST'] = $header_row[1];
-											break;
-										case 'user-agent':
-											$_SERVER['HTTP_USER_AGENT'] = $header_row[1];
-											break;
-										case 'cookie':
-											$_SERVER['HTTP_COOKIE'] = $header_row[1];
-											break;
-										case 'connection':
-											$_SERVER['HTTP_CONNECTION'] = $header_row[1];
-											break;
-										case 'cache-control':
-											$_SERVER['HTTP_CACHE_CONTROL'] = $header_row[1];
-											break;
-										case 'accept':
-											$_SERVER['HTTP_ACCEPT'] = $header_row[1];
-											break;
-										case 'accept-language':
-											$_SERVER['HTTP_ACCEPT_LANGUAGE'] = $header_row[1];
-											break;
-										case 'accept-encoding':
-											$_SERVER['HTTP_ACCEPT_ENCODING'] = $header_row[1];
-											break;
+						if(count($request_header_array) > 1) {
+							foreach($request_header_array AS $request_header_row) {
+								$request_header_row = explode(':', trim($request_header_row), 2);
+								if(count($request_header_row) > 1) {
+									$request_header_row[0] = strtoupper(trim($request_header_row[0]));
+									$request_header_row[1] = trim($request_header_row[1]);
+									if(in_array($request_header_row[0], array('HOST', 'USER-AGENT', 'COOKIE', 'CONNECTION', 'CACHE-CONTROL', 'ACCEPT', 'ACCEPT-LANGUAGE', 'ACCEPT-ENCODING'))) {
+										$_SERVER['HTTP_'.str_replace('-', '_', $request_header_row[0])] = $request_header_row[1];
 									}
 								}
 							}
 						}
-						$headers[] = 'Content-Type: '.$mime_types['html'].'; charset=utf-8';
+						response_header('Content-Type: '.$mime_types['html'].'; charset=utf-8');
 						console_log('Eval php code, namespace ID: '.$eval_namespace);
 						ob_start();
 							if(@eval('namespace EvalSpace\id'.($eval_namespace++).';?>'.$content) === false) {
@@ -185,10 +186,10 @@ while(true) {
 						ob_end_clean();
 					}
 					else if(!empty($mime_types[$type])) {
-						$headers[] = 'Content-Type: '.$mime_types[$type];
+						response_header('Content-Type: '.$mime_types[$type]);
 						console_log('Response with mime.type: '.$mime_types[$type]);
 					}
-					$response = create_response($content, 'HTTP/1.0 200 OK', $headers);
+					$response = create_response($content, 'HTTP/1.0 200 OK', $response_headers);
 				}
 				else {
 					$response = create_response(FASTPHPWEB_404, 'HTTP/1.0 404 Not Found');
@@ -204,9 +205,14 @@ while(true) {
 		}
 
 		fclose($connect);
+
 		unset($connects[array_search($connect, $connects)]);
-		unset($headers);
+		unset($response_headers);
+		unset($request);
 		unset($content);
+
+		$momory_size = memory_get_peak_usage();
+		console_log('Peak of memory: ' . @round($momory_size/pow(1024,($i=floor(log($momory_size, 1024)))), 2).$memory_unit[$i]);
 	}
 }
 
--- web/index.php
+++ web/index.php
@@ -1,3 +1,6 @@
+<?php
+header('Cache-Control: Max-Age=3600');
+?>
 <html>
 <head>
 	<title>FastPHPWeb</title>
@@ -10,6 +13,7 @@
 	Download: <a href="/test.zip">test.zip</a><br />
 	Current date & time: <?php echo date("d.m.Y, H:i:s");?><br />
 	Remote Address: <?php echo $_SERVER['REMOTE_ADDR'].':'.$_SERVER['REMOTE_PORT'];?><br />
+	__FILE__: <?php echo __FILE__;?><br />
 	User Agent: <?php echo $_SERVER['HTTP_USER_AGENT'];?><br />
 	<br />
 	<hr />
